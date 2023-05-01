56d5ed31c0e54688e08d95896430e4848f2b61fc
Timothy Erckert <timothyerckert@gmail.com>
2023-05-01 10:21:07 -0400

    Removing pi only library access to run local debug

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import eel
-import rfcontroller
+# import rfcontroller
 import json
 
-controller = rfcontroller.RFController() 
+# controller = rfcontroller.RFController() 
 
 @eel.expose
 def togglePlug(command):
-    # print(command)
-    controller.sendcode(command)
+    print(command)
+    # controller.sendcode(command)
 
 @eel.expose
 def storeConfig(setting, value):
@@ -27,4 +27,5 @@ def loadConfig():
 
 if __name__ == "__main__":
     eel.init('web', allowed_extensions=[".js",".html"])
-    eel.start('index.html', cmdline_args=['--start-fullscreen'])
+    # eel.start('index.html', cmdline_args=['--start-fullscreen'])
+    eel.start('index.html', size=(600, 800))
